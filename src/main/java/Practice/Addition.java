a82f125ec5e5d42765df2b6fb115e65754e5be3b
hnanjala <hnanjala@EXPLTDC1PBT8H4W.expressco.com>
2017-10-31 15:17:14 -0400

    Created a new class 'Addition'

---
+++ src/main/java/Practice/Addition.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Addition {
+	@Test
+public int addition()
+{
+	int a=200;
+	int b=8;
+	System.out.println("Output:"+(a+b));
+	return (a+b);
+}
+}
